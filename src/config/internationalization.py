7f7043483275e7149dfd5ad58aa0f64bd4691646
kaikeventura <kaike.gnr@hotmail.com>
2024-03-24 03:16:42 -0300

    Criado enum para lidar com internacionalização

---
+++ src/config/internationalization.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class Language(Enum):
+    en_US = "English - United States"
+    en_GB = "English - United Kingdom"
+    fr_FR = "French - France"
+    de_DE = "German - Germany"
+    de_AT = "German - Austria"
+    de_CH = "German - Switzerland"
+    it_IT = "Italian - Italy"
+    es_ES = "Spanish - Spain"
+    es_MX = "Spanish - Mexico"
+    es_AR = "Spanish - Argentina"
+    pt_PT = "Portuguese - Portugal"
+    pt_BR = "Portuguese - Brazil"
+    pt_AO = "Portuguese - Angola"
+    pt_MZ = "Portuguese - Mozambique"
+    ja_JP = "Japanese - Japan"
+    zh_CN = "Chinese - China"
+    zh_TW = "Chinese - Taiwan"
